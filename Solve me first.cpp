beb9154042e678eead8a477cdfc61a5526543229
Yash Mittal <47789628+mittalyash394@users.noreply.github.com>
2020-09-29 08:43:25 +0530

    Add files via upload

---
+++ Solve me first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int firstinput,secondinput,sum;
+    cin >> firstinput;
+    cin >> secondinput;
+    sum=firstinput+secondinput;
+    cout << sum ;
+}
+
